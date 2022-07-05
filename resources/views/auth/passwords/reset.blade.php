1255a65ea2995f0a7dcb29dae9cbe7744defef93
Petar Simonovic <simonovicp12@gmail.com>
2022-07-05 17:15:33 +0200

    done: custom password reset pages

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,105 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<head>
+    <!-- Meta -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="content-language" content="en" />
+    <meta name="description" content="ICT Cortex Library - project for high school students..." />
+    <meta name="keywords" content="ict cortex, cortex, bild, bildstudio, highschool, students, coding" />
+    <meta name="author" content="bildstudio" />
+    <!-- End Meta -->
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+    <!-- Title -->
+    <title>Reset password | Library - ICT Cortex student project</title>
+    <link rel="shortcut icon" href="{{ asset('img/library-favicon.ico') }}" type="image/vnd.microsoft.icon" />
+    <!-- End Title -->
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+    <!-- Styles -->
+    @include('includes/layout/styles')
+    <!-- End Styles -->
+</head>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+<body>
+<!-- Main content -->
+<main class="h-screen small:hidden bg-login">
+    <div class="flex items-center justify-center pt-[13%]">
+        <div class="w-full" style="max-width: 550px">
+            <form class="px-12 pt-6 pb-4 mb-4 bg-white rounded shadow-lg" method="POST" action="{{ route('password.update') }}">
+                @csrf
+                @method('POST')
+                <input type="hidden" name="token" value="{{ $token }}">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <div class="flex justify-center py-2 mb-4 text-2xl text-gray-800 border-b-2">
+                    Online Biblioteka - Type new password
                 </div>
-            </div>
+
+                @error('email')
+                        <span class="invalid-feedback text-sm text-red-500" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                @enderror
+
+                <div class="mb-4">
+                    <label class="block mb-2 text-sm font-normal text-gray-700" for="email">
+                        {{ __('Email Address') }}
+                    </label>
+
+                    <input
+                        id="email" class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                        name="email"  type="email" required autofocus placeholder="Email" value="{{ $email ?? old('email') }}">
+                    @error('email')
+                        <span class="invalid-feedback text-sm text-red-500" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 text-sm font-normal text-gray-700" for="password">
+                        {{ __('Password') }}
+                    </label>
+                    <input
+                        id="password" class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                        type="password" placeholder="Password" name="password" required autocomplete="new-password"/>
+
+                    @error('password')
+                        <span class="invalid-feedback text-sm text-red-500" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label for="password-confirm" class="block mb-2 text-sm font-normal text-gray-700">{{ __('Confirm Password') }}</label>
+                    <input
+                        id="password-confirm" class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                        type="password" placeholder="Password" name="password_confirmation" required autocomplete="new-password"/>
+                </div>
+
+                <div class="flex items-center justify-between">
+                    <button type="submit" class="inline-block px-4 py-2 text-white bg-blue-500 rounded shadow-lg btn-animation hover:bg-blue-600 focus:bg-blue-700">
+                        {{ __('Reset Password') }}
+                    </button>
+                </div>
+                <p class="text-xs text-center mt-[30px] text-gray-500">
+                    &copy;2022 ICT Cortex. All rights reserved.
+                </p>
+            </form>
         </div>
     </div>
-</div>
-@endsection
+</main>
+<!-- End Main content -->
+
+<!-- Notification for small devices -->
+@include('includes/layout/inProgress')
+
+<!-- Scripts -->
+@include('includes/layout/scripts')
+<!-- End Scripts -->
+
+</body>
+
+</html>
