3047cb7d13e8d7028e3e3a345ed3089522feeff0
Petar Simonovic <simonovicp12@gmail.com>
2022-07-17 22:48:14 +0200

    fix: db seeding

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -22,9 +23,9 @@ class UserFactory extends Factory
             'surname' => fake()->lastName,
             'email' => fake()->safeEmail(),
             'username' => fake()->userName,
-            'role_id' => '1',
+            'role_id' => Role::create(['name' => 'bibliotekar']),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$rqOUfrPxmcjIYDctSZHy4O.KTPgTDvRo0c3chZIc8gikGsq9V2gFW', // password
             'remember_token' => Str::random(10),
         ];
     }
