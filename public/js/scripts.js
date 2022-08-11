1365d2163502ee9d24b0ba79dcd49b4d04cdd3c8
Petar Simonovic <simonovicp12@gmail.com>
2022-08-11 22:22:24 +0200

    removed annoying verification on authors

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3101,24 +3101,3 @@ function checkMakeSure(event, button) {
         $('.modal:visible').each(centerModal);
     });
 }(jQuery));
-
-/* center modal */
-// function centerModals($element) {
-//     var $modals;
-//     if ($element.length) {
-//         $modals = $element;
-//     } else {
-//         $modals = $('.modal-vcenter:visible');
-//     }
-//     $modals.each( function(i) {
-//         var $clone = $(this).clone().css('display', 'block').appendTo('body');
-//         var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2);
-//         top = top > 0 ? top : 0;
-//         $clone.remove();
-//         $(this).find('.modal-content').css("margin-top", top);
-//     });
-// }
-// $('.modal-vcenter').on('show.bs.modal', function(e) {
-//     centerModals($(this));
-// });
-// $(window).on('resize', centerModals);
