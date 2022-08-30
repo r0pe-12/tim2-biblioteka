8a58ce7d6f923f683d62050c03071558b457a2ec
Petar Simonovic <simonovicp12@gmail.com>
2022-08-30 02:18:08 +0200

    fix: slika se ne prikazuje bug kad ajax radi

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3651,7 +3651,7 @@ function izdavanjeActivity(borrow) {
     var div = `
         <div class="flex flex-row mb-[30px]">
             <div class="flex w-[60px] h-[60px] items-center">
-                <img class="rounded-full" src="${borrow.librarian.photoPath == null ? '/img/profile.jpg' : borrow.librarian.photoPath}" alt="">
+                <img class="rounded-full" src="${borrow.librarian.photoPath == null ? '/img/profile.jpg' : '/storage/images/users/'+borrow.librarian.photoPath}" alt="">
             </div>
             <div class="ml-[15px] mt-[5px] flex flex-col">
                     <div class="text-gray-500 mb-[5px]">
@@ -3697,7 +3697,7 @@ function rezervacijeActivity(res) {
         case 1:
             div = `<div class="flex flex-row mb-[30px]">
                 <div class="w-[60px] h-[60px]">
-                    <img class="rounded-full" src="${res.librarian.photoPath == null ? '/img/profile.jpg' : res.librarian.photoPath}" alt="">
+                    <img class="rounded-full" src="${res.librarian.photoPath == null ? '/img/profile.jpg' : '/storage/images/users/'+res.librarian.photoPath}" alt="">
                 </div>
                 <div class="ml-[15px] mt-[5px] flex flex-col">
                     <div class="text-gray-500 mb-[5px]">
@@ -3734,7 +3734,7 @@ function rezervacijeActivity(res) {
         case 2:
             div = `<div class="flex flex-row mb-[30px]">
                 <div class="w-[60px] h-[60px]">
-                    <img class="rounded-full" src="${res.librarian.photoPath == null ? '/img/profile.jpg' : res.librarian.photoPath}" alt="">
+                    <img class="rounded-full" src="${res.student.photoPath == null ? '/img/profile.jpg' : '/storage/images/users/'+res.student.photoPath}" alt="">
                 </div>
                 <div class="ml-[15px] mt-[5px] flex flex-col">
                     <div class="text-gray-500 mb-[5px]">
@@ -3766,7 +3766,7 @@ function rezervacijeActivity(res) {
         case 3:
             div = `<div class="flex flex-row mb-[30px]">
                 <div class="w-[60px] h-[60px]">
-                    <img class="rounded-full" src="${res.librarian.photoPath == null ? '/img/profile.jpg' : res.librarian.photoPath}" alt="">
+                    <img class="rounded-full" src="${res.librarian.photoPath == null ? '/img/profile.jpg' : '/storage/images/users/'+res.librarian.photoPath}" alt="">
                 </div>
                 <div class="ml-[15px] mt-[5px] flex flex-col">
                     <div class="text-gray-500 mb-[5px]">
@@ -3803,7 +3803,7 @@ function rezervacijeActivity(res) {
         case 4:
             div = `<div class="flex flex-row mb-[30px]">
                 <div class="w-[60px] h-[60px]">
-                    <img class="rounded-full" src="${res.librarian.photoPath == null ? '/img/profile.jpg' : res.librarian.photoPath}" alt="">
+                    <img class="rounded-full" src="${res.librarian.photoPath == null ? '/img/profile.jpg' : '/storage/images/users/'+res.librarian.photoPath}" alt="">
                 </div>
                 <div class="ml-[15px] mt-[5px] flex flex-col">
                     <div class="text-gray-500 mb-[5px]">
