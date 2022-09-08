635d9d5e955ed2a92c5707c1113468b1ac2ef588
Petar Simonovic <simonovicp12@gmail.com>
2022-09-08 10:43:52 +0200

    fix bad query for activity tab

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -44,8 +44,12 @@ class DashboardController extends Controller
             $reservations->whereIn('student_id', $ucenik);
         });
         \request()->whenFilled('bibliotekar', function ($bibliotekar) use ($borrows, $reservations) {
-            $borrows->whereIn('librarian_id', $bibliotekar)->orWhereIn('librarian1_id', $bibliotekar);
-            $reservations->whereIn('librarian_id', $bibliotekar)->orWhereIn('librarian1_id', $bibliotekar);
+            $borrows->where(function ($q) use ($bibliotekar) {
+                $q->whereIn('librarian_id', $bibliotekar)->orWhereIn('librarian1_id', $bibliotekar);
+            });
+            $reservations->where(function ($q) use ($bibliotekar) {
+                $q->whereIn('librarian_id', $bibliotekar)->orWhereIn('librarian1_id', $bibliotekar);
+            });
         });
         \request()->whenFilled('knjiga', function ($knjiga) use ($borrows, $reservations) {
             $borrows->whereIn('book_id', $knjiga);
