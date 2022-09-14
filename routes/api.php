fb6c307901c5174355060dddf18caef32db58d08
Petar Simonovic <simonovicp12@gmail.com>
2022-09-14 21:54:20 +0200

    done login and register with api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login')->name('api-login');
+    Route::post('register', 'register')->name('api-register');
+});
+
+Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
     return $request->user();
 });
