71bf0dffd898d7e0fd1fb549eac8c1b34da41137
Petar Simonovic <simonovicp12@gmail.com>
2022-09-16 01:08:33 +0200

    created table for reviews

---
+++ database/migrations/2022_09_16_010621_create_book_reviews_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_reviews', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('student_id')->constrained('users', 'id')->cascadeOnDelete();
+
+            $table->text('body');
+            $table->tinyInteger('star');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_reviews');
+    }
+};
