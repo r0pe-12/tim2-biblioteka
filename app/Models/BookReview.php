913e4cc5cc8280e844f94d0da38ae6e7374feb69
Petar Simonovic <simonovicp12@gmail.com>
2022-09-16 01:14:18 +0200

    defined relationships for reviews

--- app/Models/BookReview.php
+++ app/Models/BookReview.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class BookReview extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function student(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        # code
+        return $this->belongsTo(Student::class);
+    }
+
+    public function book(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        # code
+        return $this->belongsTo(Book::class);
+    }
 }
