45cd3220e85e934ed047284b90960899fa7dbc43
Petar Simonovic <simonovicp12@gmail.com>
2022-09-30 03:02:50 +0200

    photo align try 2

--- resources/views/components/client-books-card.blade.php
+++ resources/views/components/client-books-card.blade.php
@@ -5,14 +5,14 @@
 <div style="margin: auto" class="col-auto">
     <div class="card" style="width: 18rem;height: auto">
         <a style="height: 461px" href="#knjigaPrikaz">
-            <img style="object-fit: cover;height: 100%; width: 100%"
+            <img style="object-fit: contain;height: 100%; width: 100%"
                  src="{{ $book->cover() }}" class="book-image" alt="...">
         </a>
         <div class="card-body">
             <h2 class="card-title" style="font-size: x-large"><a href="#knjigaPrikaz">{{ $book->title }}</a>
             </h2>
             <div class="card-text" style="min-height: 120px">
-                {{ Str::limit($book->description, 100) }}
+                {!! strip_tags(Str::limit($book->description, 100)) !!}
             </div>
             <div class="d-grid gap-2">
                 <a href="#rezervisiKnjigu" class="btn btn-prem">Rezerviši</a>
