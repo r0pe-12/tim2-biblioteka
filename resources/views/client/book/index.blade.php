8c836288de2c62062c1cc4d23bfca89912eff647
Petar Simonovic <simonovicp12@gmail.com>
2022-10-01 02:08:12 +0200

    added search bar in books page - not functional

--- resources/views/client/book/index.blade.php
+++ resources/views/client/book/index.blade.php
@@ -2,9 +2,23 @@
     @section('scripts')
         <script class="u-script" type="text/javascript" src="{{ asset('js/landing/reserveBoook.js') }}" crossorigin="anonymous"></script>
     @endsection
-    <div class="container rounded bg-white mt-5 mb-5">
+    <div class="container rounded bg-white mb-5">
         <div class="card-body">
             <div class="row">
+                <div class="col-sm-6 offset-sm-3 mt-3">
+                    <form onsubmit="event.preventDefault()" id="searchBookForm">
+                        <div class="input-group">
+                            <input class="form-control border-end-0 border rounded-pill" type="search" placeholder="Search" id="searchBar" autocomplete="off" name="searchWord">
+                            <span class="input-group-append">
+                                <button class="btn btn-outline-secondary bg-white border-bottom-0 border rounded-pill ms-n5" type="submit">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="row mt-5">
 {{--                <div class="col-md-3">--}}
 
 {{--                </div>--}}
