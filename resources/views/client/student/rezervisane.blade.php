1ee8b169d3f6b0b01394295b0a201989dcf34cbe
Petar Simonovic <simonovicp12@gmail.com>
2022-10-03 01:33:37 +0200

    added sexy bs tabs to reservation grid for student

--- resources/views/client/student/rezervisane.blade.php
+++ resources/views/client/student/rezervisane.blade.php
@@ -6,80 +6,84 @@
                 <div class="col-md-12">
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Aktivne rezervacije</h6>
+                            <nav>
+                                <div class="nav nav-pills" id="nav-tab" role="tablist">
+                            <a class="nav-link active" id="arhvirane" data-bs-toggle="tab" href="#aktivne-tab"  role="tab" aria-controls="aktivne" aria-selected="true">Aktivne rezervacije</a>
+                            <a class="nav-link" id="arhivirane" data-bs-toggle="tab" href="#arhivirane-tab"  role="tab" aria-controls="arhivirane" aria-selected="false">Arhivirane rezervacije</a>
+                                </div>
+                            </nav>
                         </div>
-
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped" id="dataTable" width="100%" cellspacing="0">
-                                    <thead>
-                                        <tr>
-                                            <th>Knjiga</th>
-                                            <th>Datum rezervacije</th>
-                                            <th>Rezervacija ističe</th>
-                                            <th>Status</th>
-                                            <th>Akcija</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @if(count($activeRes) > 0)
-                                            @foreach($activeRes as $reservation)
+                        <div class="tab-content" id="nav-tabContent">
+                            <div class="tab-pane fade show active" id="aktivne-tab" role="tabpanel" aria-labelledby="aktivne">
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table class="table table-striped" id="dataTable" width="100%" cellspacing="0">
+                                            <thead>
                                                 <tr>
-                                                    <td>{{ $reservation->book->title }}</td>
-                                                    <td>{{ \Carbon\Carbon::parse($reservation->submttingDate)->format('d.m.Y') }}</td>
-                                                    <td>{{ \Carbon\Carbon::parse($reservation->submttingDate)->addDays($res_deadline->value)->format('d.m.Y') }}</td>
-                                                    <td>{{ $reservation->status }}</td>
-                                                    <td><button class="btn btn-outline-danger">Otkaži</button></td>
+                                                    <th>Knjiga</th>
+                                                    <th>Datum rezervacije</th>
+                                                    <th>Rezervacija ističe</th>
+                                                    <th>Status</th>
+                                                    <th>Akcija</th>
                                                 </tr>
-                                            @endforeach
-                                        @else
-                                            <tr>
-                                                <td class="text-center" colspan="5">Nemate aktivnih rezervacija</td>
-                                            </tr>
-                                        @endif
-                                    </tbody>
-                                </table>
+                                            </thead>
+                                            <tbody>
+                                                @if(count($activeRes) > 0)
+                                                    @foreach($activeRes as $reservation)
+                                                        <tr>
+                                                            <td>{{ $reservation->book->title }}</td>
+                                                            <td>{{ \Carbon\Carbon::parse($reservation->submttingDate)->format('d.m.Y') }}</td>
+                                                            <td>{{ \Carbon\Carbon::parse($reservation->submttingDate)->addDays($res_deadline->value)->format('d.m.Y') }}</td>
+                                                            <td>{{ $reservation->status }}</td>
+                                                            <td><button class="btn btn-outline-danger">Otkaži</button></td>
+                                                        </tr>
+                                                    @endforeach
+                                                @else
+                                                    <tr>
+                                                        <td class="text-center" colspan="5">Nemate aktivnih rezervacija</td>
+                                                    </tr>
+                                                @endif
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
 
-                    <div class="card shadow mb-4" style="margin-top: 100px">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Arhivirane rezervacije</h6>
-                        </div>
-
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped" id="dataTable" width="100%" cellspacing="0">
-                                    <thead>
-                                        <tr>
-                                            <th>Knjiga</th>
-                                            <th>Datum rezervacije</th>
-                                            <th>Rezervacija zatvorena</th>
-                                            <th>Status</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @if(count($archiveRes) > 0)
-                                            @foreach($archiveRes as $reservation)
-                                                <tr>
-                                                    <td>{{ $reservation->book->title }}</td>
-                                                    <td>{{ \Carbon\Carbon::parse($reservation->submttingDate)->format('d.m.Y') }}</td>
-                                                    <td>{{ \Carbon\Carbon::parse($reservation->closingDate)->format('d.m.Y') }}</td>
-                                                    <td>
-                                                        <div class="inline-block px-[6px] py-[2px] font-medium {{ $reservation->closingReason_id == \App\Models\ClosingReason::BOOK_BORROWED ? 'bg-green-200' : 'bg-red-200' }} rounded-[10px]">
-                                                            <span class="{{ $reservation->closingReason_id == \App\Models\ClosingReason::BOOK_BORROWED ? 'text-green-800' : 'text-red-800' }}">{{ $reservation->cReason }}</span>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        @else
+                            <div class="tab-pane fade" id="arhivirane-tab" role="tabpanel" aria-labelledby="arhvirane">
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table class="table table-striped" id="dataTable" width="100%" cellspacing="0">
+                                            <thead>
                                             <tr>
-                                                <td class="text-center" colspan="4">Nemate arhiviranih rezervacija</td>
+                                                <th>Knjiga</th>
+                                                <th>Datum rezervacije</th>
+                                                <th>Rezervacija zatvorena</th>
+                                                <th>Status</th>
                                             </tr>
-                                        @endif
-                                    </tbody>
-                                </table>
+                                            </thead>
+                                            <tbody>
+                                            @if(count($archiveRes) > 0)
+                                                @foreach($archiveRes as $reservation)
+                                                    <tr>
+                                                        <td>{{ $reservation->book->title }}</td>
+                                                        <td>{{ \Carbon\Carbon::parse($reservation->submttingDate)->format('d.m.Y') }}</td>
+                                                        <td>{{ \Carbon\Carbon::parse($reservation->closingDate)->format('d.m.Y') }}</td>
+                                                        <td>
+                                                            <div class="inline-block px-[6px] py-[2px] font-medium {{ $reservation->closingReason_id == \App\Models\ClosingReason::BOOK_BORROWED ? 'bg-green-200' : 'bg-red-200' }} rounded-[10px]">
+                                                                <span class="{{ $reservation->closingReason_id == \App\Models\ClosingReason::BOOK_BORROWED ? 'text-green-800' : 'text-red-800' }}">{{ $reservation->cReason }}</span>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            @else
+                                                <tr>
+                                                    <td class="text-center" colspan="4">Nemate arhiviranih rezervacija</td>
+                                                </tr>
+                                            @endif
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
