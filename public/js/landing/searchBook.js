3eef79f2cec5b79e7895053f6a425939fb184856
Petar Simonovic <simonovicp12@gmail.com>
2022-10-05 12:42:35 +0200

    fix url to book details on search, forgot to add href in ajax

--- public/js/landing/searchBook.js
+++ public/js/landing/searchBook.js
@@ -54,12 +54,12 @@ function noAuth(book) {
     var card =
             `<div style="margin: auto" class="col-auto">
                 <div class="card" style="width: 18rem;height: auto">
-                    <a style="height: 461px" href="#knjigaPrikaz">
+                    <a style="height: 461px" href="/knjige/${book.id}">
                         <img style="object-fit: contain;height: 100%; width: 100%"
                              src="${book.photo}" class="book-image" alt="...">
                     </a>
                     <div class="card-body">
-                        <h2 class="card-title" style="font-size: x-large"><a href="#knjigaPrikaz">${book.title}</a>
+                        <h2 class="card-title" style="font-size: x-large"><a href="/knjige/${book.id}">${book.title}</a>
                         </h2>
                         <div class="card-text" style="min-height: 120px">
                             ${book.description ? book.description.slice(0, 100).replace(/<\/?[^>]+(>|$)/g, "") + '...' : ''}
@@ -81,12 +81,12 @@ function auth(book) {
     var card =
         `<div style="margin: auto" class="col-auto">
                 <div class="card" style="width: 18rem;height: auto">
-                    <a style="height: 461px" href="#knjigaPrikaz">
+                    <a style="height: 461px" href="/knjige/${book.id}">
                         <img style="object-fit: contain;height: 100%; width: 100%"
                              src="${book.photo}" class="book-image" alt="...">
                     </a>
                     <div class="card-body">
-                        <h2 class="card-title" style="font-size: x-large"><a href="#knjigaPrikaz">${book.title}</a>
+                        <h2 class="card-title" style="font-size: x-large"><a href="/knjige/${book.id}">${book.title}</a>
                         </h2>
                         <div class="card-text" style="min-height: 120px">
                             ${book.description ? book.description.slice(0, 100).replace(/<\/?[^>]+(>|$)/g, "") + '...' : ''}
